2dd0223df9206c6b59703f19307f2536335c5013
javierCel35864 <celestino.a.javi@gmail.com>
2025-03-20 15:23:45 -0500

    project 1 complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// javascript
+// increment count when "INCREMENT" button is cliked
+// in the button element from html add an onclick action that connects to a 
+//  fucntion in this file called "increment"
+let count = 0
+//variable for stand in for the elemnet that displays count in html
+let countEl = document.getElementById("count-el")
+let savedLs = document.getElementById("saved-ls")
+function increment(){
+    count += 1
+    //change tect of count-el in html to count variable
+    countEl.innerText = count
+    
+}
+
+function save(){
+    
+    //pint in cosole
+    console.log(count);
+}
+
+function history(){
+    let countStr = count + " - "
+    //display the history of saved values in browser
+    savedLs.innerText += countStr
+    count = 0
+    countEl.innerText = count
+}
+//test segment
+//increment();
+//console.log(count)
